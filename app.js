12669d0b4b7027849ef8c44c3ed7ed1228fa4b84
gerard <gerard.cano.7e6@itb.cat>
2023-05-09 10:43:05 +0200

    aaaa

--- app.js
+++
@@ -1,8 +0,0 @@
-    let plus = document.querySelector(".plus-hour"),
-            minus = document.querySelector(".minus-hour"),
-            num = document.querySelector(".num-hour");
-    let a = 1;
-    plus.addEventListener("click", ()=>{
-      a++;
-      console.log(a);
-    });
