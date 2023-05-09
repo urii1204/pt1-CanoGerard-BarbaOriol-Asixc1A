c82c02846d7294a6a10d08e66f97a57e54c89a17
oriol_barba <oriol.barba.7e6@itb.cat>
2023-05-09 11:53:25 +0200

    Commited

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
     let plus = document.querySelector(".plus-hour"),
             minus = document.querySelector(".minus-hour"),
             num = document.querySelector(".num-hour");
-    let a = 1;
+    let a = 0;
+    let plus_min = document.querySelector(".plus-minute"),
+            minus_min = document.querySelector(".minus-minute"),
+            num_min = document.querySelector(".num-minute");
+    let b = 0;
     plus.addEventListener("click", ()=>{
-        if (a < 24){
+        if (a < 24 && a >= 0){
             a++;
             if (a<10){
                 a = "0"+a;
@@ -11,8 +15,37 @@
         num.innerText = a;
 
         }
+    });
+        minus.addEventListener("click", ()=>{
+        if (a <= 24 && a > 0){
+            a--;
+            if (a<10){
+                a = "0"+a;
+            }
+        num.innerText = a;
+        }
 
     });
 
+    plus_min.addEventListener("click", ()=>{
+        if (b < 59 && b >= 0){
+            b++;
+            if (b<10){
+                b = "0"+b;
+            }
+        num_min.innerText = b;
+
+        }
+    });
+        minus_min.addEventListener("click", ()=>{
+        if (b <= 59 && b > 0){
+            b--;
+            if (b<10){
+                b = "0"+b;
+            }
+        num_min.innerText = b;
+        }
+
+    });
 
 
