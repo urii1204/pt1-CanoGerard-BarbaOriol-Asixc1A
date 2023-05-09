d6875cfb1f01353cf5fcfa7d0e6ffb50621f1989
oriol_barba <oriol.barba.7e6@itb.cat>
2023-05-09 09:21:08 +0200

    Commited

---
+++ app.js
@@ -0,0 +1,8 @@
+    let plus = document.querySelector(".plus-hour"),
+            minus = document.querySelector(".minus-hour"),
+            num = document.querySelector(".num-hour");
+    let a = 1;
+    plus.addEventListener("click", ()=>{
+      a++;
+      console.log(a);
+    });
